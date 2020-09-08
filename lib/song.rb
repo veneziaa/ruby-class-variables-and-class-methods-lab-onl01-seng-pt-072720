7cab1edd5759d00824293a8bf18288c1e5b6613a
Venice Vargas <venicevargas1@live.com>
2020-09-08 22:09:44 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,43 @@
+class Song 
+  
+   attr_accessor :name, :artist, :genre
+   
+  @@song_count=0
+  @@genres=[]
+  @@artists=[]
+  
+  def initialize (name,artist,genre)
+    @name=name
+    @artist=artist
+    @genre=genre
+    @@genres<<@genre
+    @@artists<<@artist
+    @@song_count += 1 
+    
+  end 
+  
+  def self.count
+    @@song_count
+  end
+  
+  def self.genres
+    @@genre.uniq
+  end
+  
+  def self.artists
+    @@artists.uniq
+  end
+  
+  def self.genre_count
+  genre_count = {}
+  @genres.each do |genre|
+    if genre_count[genre]
+      genre_count[genre]+=1 
+    else
+      genre_count[genre]=1
+    end
+  end
+  genre_count
+end 
+  
+ 
