f29b1042c80eaf8dfb8f6b14b1da4bfc4b1a41e0
Venice Vargas <venicevargas1@live.com>
2020-09-08 22:19:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -53,3 +53,11 @@ class Song
   end
 
 end
+
+ninety_nine_problems = Song.new("99 Problems", "Jay-Z", "Rap")
+mask_off = Song.new("Mask Off", "Future", "Rap")
+dance_macabre = Song.new("Dance Macabre", "Ghost", "Metal")
+if_you_have_ghosts = Song.new("If You Have Ghosts", "Ghost", "Metal")
+Song.genres
+p Song.artist_count
+p Song.genre_count
