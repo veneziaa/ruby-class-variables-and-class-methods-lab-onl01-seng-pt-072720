f63618956134e3209cf0b541f46b80610ac9f316
Venice Vargas <venicevargas1@live.com>
2020-09-08 22:24:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,55 +1,54 @@
+(1..5).each_with_object(2) { |value, memo| memo *= value} return memo
 
-class Song
 
-  attr_accessor :name, :artist, :genre
-
-  @@song_count = 0
+class Song 
+  @@count = 0
+  
+  def self.count 
+    @@count
+  end 
+  
   @@genres = []
-  @@artists = []
+  
 
-  def initialize(name, artist, genre)
-    @name = name
-    @artist = artist
-    @genre = genre
-    @@genres << @genre
-    @@artists << @artist
-    @@song_count +=1
-  end
-
-  def self.count
-    @@song_count
-  end
-
+  
   def self.genres
     @@genres.uniq
   end
-
-  def self.artists
-    @@artists.uniq
-  end
-
+  
+  def self.genre_count
+    @@genres.inject(Hash.new(0)) { |total, i| total[i] += 1 ;total}
+  end 
+  
   def self.genre_count
     genre_count = {}
-    @@genres.each do |genre| 
+    @@genres.each do |genre|
       if genre_count[genre]
-      genre_count[genre] += 1 
+        genre_count[genre] += 1 
       else
-      genre_count[genre] = 1
+        genre_count[genre] = 1
       end
     end
     genre_count
   end
-
+  
+  @@artists = []
+  
   def self.artist_count
-    artist_count = {}
-    @@artists.each do |artist|
-      if artist_count[artist]
-      artist_count[artist] +=1
-      else
-      artist_count[artist] = 1
-      end
-    end
-    artist_count
+    @@artists.inject(Hash.new(0)) { |total, i| total[i] += 1 ;total}
   end
-
-end
+  
+  def self.artists
+    @@artists.uniq
+  end
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @@artists << artist
+    @genre = genre
+    @@genres << genre
+    @@count += 1
+  end
+  attr_accessor :name, :artist, :genre
+end 
