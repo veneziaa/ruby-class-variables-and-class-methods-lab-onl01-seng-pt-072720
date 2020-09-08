e5c0ceb2b279ef75e4e30fdb28649c867c9510b1
Venice Vargas <venicevargas1@live.com>
2020-09-08 22:18:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -44,12 +44,12 @@ class Song
     artist_count = {}
     @@artists.each do |artist|
       if artist_count[artist]
-        artist_count[artist] += 1 
-        else
-          artist_count[artist] = 1 
-          end
+      artist_count[artist] += 1 
+      else
+        artist_count[artist] = 1 
         end
-        artist_count
       end
-      end
+      artist_count
+  end
+end
  
